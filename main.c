1ca107f73f15cdffd29279909f1f510e1be7a328
leojj <leocj@hotmail.com>
2020-01-02 23:51:58 -0500

    Task_0

--- main.c
+++ main.c
@@ -1,55 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "monty.h"
 
 /**
- * find_keyword - Entry point
+ * token_opcode - reads line and tokenize for opcode an argumen if any.
+ * @line: pointer to line with opcodes.
  *
- * Return: Always 0 (Success)
+ * Return: pointer to array of pointers with opcode and arguments if any found.
  */
-int find_keyword(char **line)
+char **token_opcode(char *line)
 {
-    int n = 0;
-    while (( (*line)[n]  != '\0') && ((*line)[n] == ' '))
-	n++;
-    return n;
-}
+	const char s[7] = " \t\r\n\v\f";
+	char **result;
 
+	result = malloc(2 * sizeof(char *));
+	if (result == NULL)
+		return (NULL);
+
+	/**
+	 * Takes first token found as opcode and store it in result[0].
+	 * Takes second token (if any) as argument and store it in result[0].
+	 */
+	result[0] = strtok(line, s);
+	result[1] = strtok(NULL, s);
+
+	return (result);
+}
 
 /**
  * main - Entry point
+ * @argc: number of arguments passed as parameter to main program.
+ * @argv: array of strings with the parameters passed to main program
  *
- * Return: Always 0 (Success)
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on any failure.
  */
-
 int main(int argc, char *argv[])
 {
-   FILE *stream;
-   char *line = NULL;
-   size_t len = 0, len2 = 0;
-   ssize_t nread;
+	FILE *stream = NULL;
+	char *line = NULL, **opcode = NULL;
+	size_t len = 0;
+	ssize_t nread = 0;
 
-   if (argc != 2)
-   {
-       fprintf(stderr, "USAGE: monty file\n");
-       exit(EXIT_FAILURE);
-   }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-   stream = fopen(argv[1], "r");
-   if (stream == NULL)
-   {
-       fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-       exit(EXIT_FAILURE);
-   }
+	stream = fopen(argv[1], "r");
+	if (stream == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-   while ((nread = getline(&line, &len, stream)) != -1)
-   {
-       printf("%s", line);
-       len2 = find_keyword(&line);
-       printf("%s - %ld\n", line, len2);
-   }
+	while ((nread = getline(&line, &len, stream)) != -1)
+	{
+		opcode = token_opcode(line);
+		printf("<%s><%s>\n", opcode[0], opcode[1]);
+	}
 
-   free(line);
-   fclose(stream);
-   exit(EXIT_SUCCESS);
+	free(line);
+	free(opcode);
+	fclose(stream);
+	exit(EXIT_SUCCESS);
 }
 
