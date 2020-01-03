d337a1c4c72cfaacb1ffb6e69f04e843f817e4f3
dfbq91 <dfbq91@gmail.com>
2020-01-03 16:06:39 -0500

    pop implementation

--- pop.c
+++ pop.c
@@ -1,49 +1,22 @@
 #include "monty.h"
 
 /**
- * push - pushes an element to the stack.
- * @number: value to be pushed
+ * pop - removes the top elements of the stack.
+ * @head: top of the stack
+ * @line_number: line number where a wrong command was found
  * Return: void
  */
 void pop(stack_t **head, unsigned int line_number)
 {
-	stack_t *new_node = NULL;
-	size_t len = 0, i = 0;
-
-	if (opcode[1] == NULL)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	len = strlen(opcode[1]);
-	for (i = 0; i < len; i++)
-		if (!isdigit(opcode[1][i]))
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
-		}
-
-	if (head == NULL)
-		exit(EXIT_FAILURE);
-
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	new_node->n = atoi(opcode[1]);
-	new_node->prev = NULL;
-	new_node->next = NULL;
+	stack_t *current;
 
 	if (*head == NULL)
-		*head = new_node;
-	else
 	{
-		new_node->next = *head;
-		(*head)->prev = new_node;
-		*head = new_node;
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
 	}
+
+	current = *head;
+	*head = current->next;
+	free(current);
 }
