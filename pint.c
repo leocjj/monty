8c8c1a0177ff51af40b638b6f454d233559245ab
dfbq91 <dfbq91@gmail.com>
2020-01-03 15:51:47 -0500

    pint implementation

--- pint.c
+++ pint.c
@@ -1,49 +1,19 @@
 #include "monty.h"
 
 /**
- * push - pushes an element to the stack.
- * @number: value to be pushed
+ * pint - prints the value at the top of the stack
+ * @head: top of the stack
+ * @line_number: line number where a wrong command was found
  * Return: void
  */
 void pint(stack_t **head, unsigned int line_number)
 {
-	stack_t *new_node = NULL;
-	size_t len = 0, i = 0;
-
-	if (opcode[1] == NULL)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	len = strlen(opcode[1]);
-	for (i = 0; i < len; i++)
-		if (!isdigit(opcode[1][i]))
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
-		}
-
-	if (head == NULL)
-		exit(EXIT_FAILURE);
-
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	new_node->n = atoi(opcode[1]);
-	new_node->prev = NULL;
-	new_node->next = NULL;
-
 	if (*head == NULL)
-		*head = new_node;
-	else
 	{
-		new_node->next = *head;
-		(*head)->prev = new_node;
-		*head = new_node;
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
 	}
+	stack_t *current = *head;
+
+	printf("%d\n", current->n);
 }
