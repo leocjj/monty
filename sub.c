f532581ac4e7d026b80f86653e33f0d2132d4435
dfbq91 <dfbq91@gmail.com>
2020-01-04 16:05:48 -0500

    stack NULL case fixed

--- sub.c
+++ sub.c
@@ -12,13 +12,19 @@ void _sub(stack_t **head, unsigned int line_number)
 	stack_t *current = *head;
 	int nnodes = 1; /*number of elements in stack*/
 
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
 	while (current->next != NULL)
 	{
 		current = current->next;
 		nnodes++;
 	}
 
-	if (nnodes + 1 <= 2 || *head == NULL)
+	if (nnodes + 1 <= 2)
 	{
 		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
