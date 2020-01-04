67779b49344165a31ef48ec4286f23974f8fcfb9
leojj <leocj@hotmail.com>
2020-01-04 14:44:33 -0500

    Update pop.c

--- pop.c
+++ pop.c
@@ -2,22 +2,26 @@
 
 /**
  * _pop - removes the top elements of the stack.
- * @head: double pointer to header (top) of the stack.
+ * @stack: double pointer to header (top) of the stack.
  * @line_number: counter for line number of the file.
  *
  * Return: void.
  */
-void _pop(stack_t **head, unsigned int line_number)
+void _pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current;
 
-	if (*head == NULL)
+	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		free_stack_t(*stack);
+
 		exit(EXIT_FAILURE);
 	}
 
-	current = *head;
-	*head = current->next;
+	current = *stack;
+	if ((*stack)->next)
+		(*stack)->next->prev = NULL;
+	*stack = (*stack)->next;
 	free(current);
 }
